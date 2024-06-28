6a654344dbae775aff8eb600abbb9c0a5941f4f6
Davud <david.poninka@gmail.com>
2024-06-28 14:10:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let age = 16;
+console.log(age);
+let name = "Davyd";
+console.log(name);
+let isStudent = true;
+console.log(isStudent);
+let myString = "Передивись запис";
+console.log(myString);
+let myNumber = 10;
+myNumber = myNumber + 10;
+console.log(myNumber);
+let myNull = null;
+console.log(myNull);
+let userName = prompt("Введіть ваше ім'я");
+alert(`Hello, ${userName}`);
+let userConfirmation = confirm("Ви підтверджуєте дію?");
+if (userConfirmation) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+alert("Увага! Дія небезпечна.");
+let dangerConfirmation = confirm("Ви підтверджуєте дію?");
+if (dangerConfirmation) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
